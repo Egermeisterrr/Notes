674dbdcaa1cc283e5bc19b624667b8823fbdfd26
Egor <egor.fokin.3005@mail.ru>
2022-05-20 21:56:15 +0300

    make user interface

---
+++ app/src/main/java/com/example/mynotes/EditActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mynotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+
+    fun openImageEditor(view: View) {
+
+    }
+
+    fun deleteImage(view: View) {
+
+    }
+
+    fun editImage(view: View) {
+
+    }
+}
