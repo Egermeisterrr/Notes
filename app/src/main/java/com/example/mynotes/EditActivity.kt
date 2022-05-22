0551b77e83b2a4eeb15184e95adff03a8753907d
Egor <egor.fokin.3005@mail.ru>
2022-05-22 12:46:32 +0300

    almost ready db

--- app/src/main/java/com/example/mynotes/EditActivity.kt
+++ app/src/main/java/com/example/mynotes/EditActivity.kt
@@ -1,15 +1,30 @@
 package com.example.mynotes
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mynotes.db.MyDbManager
 import kotlinx.android.synthetic.main.activity_edit.*
 
 
 class EditActivity : AppCompatActivity() {
+    val myDbManager = MyDbManager(this)
+    val imageRequestCode = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        myDbManager.openDb()
+    }
+
+    fun onClickSave(view: View) {
+        myDbManager.insertToDb(edTitle.text.toString(), edDesc.text.toString())
     }
 
     fun openImageEditor(view: View) {
@@ -22,7 +37,8 @@ class EditActivity : AppCompatActivity() {
         addImage.visibility = View.VISIBLE
     }
 
-    fun editImage(view: View) {
-
+    override fun onDestroy() {
+        super.onDestroy()
+        myDbManager.closeDb()
     }
 }
